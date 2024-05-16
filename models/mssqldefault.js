60d37f615d68f3b2e75cc3f45f4ca5ab5916c137
MattiasSp <mattias.spangmyr@gmail.com>
2024-05-16 11:56:48 +0200

    Fixed the filtering for mssql model

--- models/mssqldefault.js
+++ models/mssqldefault.js
@@ -2,13 +2,17 @@ var mssqlDefault = function mssqlDefault(queryString, queryOptions, defaultLimit
   var schema = queryOptions.schema;
   var database = queryOptions.database;
   var table = queryOptions.table;
+  var condition = queryString;
   var sqlSearchFields;
+  var sqlSearchFieldsFilter;
   if (queryOptions.searchField) {
-    sqlSearchFields = queryOptions.searchField + " AS NAMN,";
+    sqlSearchFields = queryOptions.searchField;
+    sqlSearchFieldsFilter = "LOWER(" + queryOptions.searchField + ") LIKE LOWER('" + condition + "%')";
   } else if (queryOptions.searchFields) {
-    sqlSearchFields = "CONCAT(" + queryOptions.searchFields.filter((e) => e).join(", ") + ") AS NAMN,";
-  } else {
-    sqlSearchFields = ""
+    sqlSearchFields = "CONCAT(" + queryOptions.searchFields.filter((e) => e).join(", ") + ")";
+    sqlSearchFieldsFilter = "LOWER(" + 
+      queryOptions.searchFields.filter((e) => e).join(") LIKE LOWER('" + condition + "%')\n   OR LOWER(") + 
+      ") LIKE LOWER('" + condition + "%')";
   }
   var fields = queryOptions.fields;
   var sqlFields = fields ? fields.join(',') + "," : "";
@@ -17,17 +21,16 @@ var mssqlDefault = function mssqlDefault(queryString, queryOptions, defaultLimit
   var wkt = useCentroid ? geometryField + ".STPointOnSurface().ToString() AS GEOM " + " " :
     geometryField + ".ToString() AS GEOM " + " ";
   var type = " '" + table + "'" + " AS TYPE, ";
-  var condition = queryString;
   var searchString;
   var limitNumber = queryOptions.limit || defaultLimit || 1000;
   var limit = "TOP " + limitNumber.toString() + " ";
 
   searchString =
     "SELECT " + limit +
-    sqlSearchFields + sqlFields + type + wkt +
+    sqlSearchFields + " AS NAMN," + sqlFields + type + wkt +
     " FROM " + database + "." + schema + "." + table +
-    " WHERE LOWER(" + searchField + ") LIKE LOWER('" + condition + "%')" + " " +
-    " ORDER BY " + searchField + "";
+    " WHERE " + sqlSearchFieldsFilter +
+    " ORDER BY " + sqlSearchFields;
 
   return searchString;
 }
