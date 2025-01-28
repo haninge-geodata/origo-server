23c8f836b58e981a9803584aa11b3c8b294ce7f2
MattiasSp <mattias.spangmyr@gmail.com>
2025-01-28 11:05:59 +0100

    Missed a couple of double quotes

--- models/pgdefault.js
+++ models/pgdefault.js
@@ -30,7 +30,7 @@ var pgDefault = function pgDefault(queryString, queryOptions, defaultLimit) {
   var wkt = useCentroid ? 'ST_AsText(ST_PointOnSurface(' + table + '."' + geometryField + '")) AS "GEOM" ' :
     'ST_AsText("' + table + '"."' + geometryField + '") AS "GEOM" ';
   var sqlFields = fields ? fields.join(',') + ',' : '';
-  var type = ' \'' + table + "'" + ' AS "TYPE", ';
+  var type = ' \'' + table + '\'' + ' AS "TYPE", ';
   var searchString;
   var limitNumber = queryOptions.limit || defaultLimit || 1000;
   var limit = ' LIMIT ' + limitNumber.toString() + ' ';
