474bd274077530784f87f5899f324517d8bb6cdf
MattiasSp <mattias.spangmyr@gmail.com>
2025-01-28 11:00:27 +0100

    Use single quotes consistently

--- models/pgdefault.js
+++ models/pgdefault.js
@@ -25,13 +25,12 @@ var pgDefault = function pgDefault(queryString, queryOptions, defaultLimit) {
       ') LIKE LOWER(\'' + condition + '%\')';
   }
   var fields = queryOptions.fields;
-  var geometryField = queryOptions.geometryName || "geom";
-  var useCentroid = queryOptions.hasOwnProperty("useCentroid") ? queryOptions.useCentroid : true;
+  var geometryField = queryOptions.geometryName || 'geom';
+  var useCentroid = queryOptions.hasOwnProperty('useCentroid') ? queryOptions.useCentroid : true;
   var wkt = useCentroid ? 'ST_AsText(ST_PointOnSurface(' + table + '."' + geometryField + '")) AS "GEOM" ' :
     'ST_AsText("' + table + '"."' + geometryField + '") AS "GEOM" ';
-  var sqlFields = fields ? fields.join(',') + "," : "";
-  var type = " '" + table + "'" + ' AS "TYPE", ';
-  var condition = queryString;
+  var sqlFields = fields ? fields.join(',') + ',' : '';
+  var type = ' \'' + table + "'" + ' AS "TYPE", ';
   var searchString;
   var limitNumber = queryOptions.limit || defaultLimit || 1000;
   var limit = ' LIMIT ' + limitNumber.toString() + ' ';
